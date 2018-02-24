4c51de6a6de71bae4c04b23e80502482f8061295
Tushar Gupta <tushargupta98@gmail.com>
2018-02-24 01:12:58 -0500

    Add Jacoco for code coverage report. Added new test cases to improve code coverage

--- src/test/java/com/paysafe/apimonitor/ws/business/MonitoringBOTest.java
+++ src/test/java/com/paysafe/apimonitor/ws/business/MonitoringBOTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.paysafe.apimonitor.ws.AbstractTest;
 import com.paysafe.apimonitor.ws.dao.ApplicationConstants;
@@ -20,29 +21,40 @@ import org.junit.Assert;
 
 public class MonitoringBOTest extends AbstractTest {
 
+
+	MonitoringBO mBO;
+	
+	StartRequestVO srVO;
+	
+	ExecutionResponseVO erVO;
+	ServiceMonitorDAO smDAO;
+	
+	ServiceStatusDAO ssDAO;
+	
 	@Before
 	public void setUp() {
-		ServiceMonitorDAO smDAO = MonitoringFactory.getServiceMonitorDAO(ApplicationConstants.SERVICE_MONITOR_DAO);
+		smDAO = MonitoringFactory.getServiceMonitorDAO(ApplicationConstants.SERVICE_MONITOR_DAO);
 		smDAO.cleanCache();
-		ServiceStatusDAO ssDAO = MonitoringFactory.getServiceStatusDAO(ApplicationConstants.SERVICE_STATUS_DAO);
+		ssDAO = MonitoringFactory.getServiceStatusDAO(ApplicationConstants.SERVICE_STATUS_DAO);
 		ssDAO.cleanCache();
 	}
 
 	@After
 	public void cleanUp() {
-		ServiceMonitorDAO smDAO = MonitoringFactory.getServiceMonitorDAO(ApplicationConstants.SERVICE_MONITOR_DAO);
+		smDAO = MonitoringFactory.getServiceMonitorDAO(ApplicationConstants.SERVICE_MONITOR_DAO);
 		smDAO.cleanCache();
-		ServiceStatusDAO ssDAO = MonitoringFactory.getServiceStatusDAO(ApplicationConstants.SERVICE_STATUS_DAO);
+		ssDAO = MonitoringFactory.getServiceStatusDAO(ApplicationConstants.SERVICE_STATUS_DAO);
 		ssDAO.cleanCache();
 	}
 
 	@Test
 	public void testMonitoring() {
-		MonitoringBO mBO = new MonitoringBO();
-		StartRequestVO srVO = new StartRequestVO();
-		srVO.setServiceURL("Service1");
+		srVO = new StartRequestVO();
+		srVO.setServiceURL("https://api.test.paysafe.com/accountmanagement/monitor");
 		srVO.setInterval(2);
-		ExecutionResponseVO erVO = mBO.createServiceMonitor(srVO);
+		erVO = new ExecutionResponseVO();
+		mBO = new MonitoringBO();
+		erVO = mBO.createServiceMonitor(srVO);
 		Assert.assertEquals("Monitoring did not start correctly", 1001, erVO.getCode());
 		try {
 			Thread.sleep(10000);
@@ -56,4 +68,41 @@ public class MonitoringBOTest extends AbstractTest {
 
 	}
 
+	@Test
+	public void testStartMonitoringScenarios() {
+		srVO = new StartRequestVO();
+		srVO.setServiceURL("https://api.test.paysafe.com/accountmanagement/monitor2");
+		srVO.setInterval(2);
+		erVO = new ExecutionResponseVO();
+		mBO = new MonitoringBO();
+		mBO.createServiceMonitor(srVO);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		erVO = mBO.createServiceMonitor(srVO);
+		Assert.assertEquals("Monitoring did not detect duplicate start request", 1002, erVO.getCode());
+		mBO.stopMonitoring(srVO.getServiceURL());
+		srVO.setServiceURL("https://api.test.paysafe.com/accountmanagement/monitor");
+		srVO.setInterval(-1);
+		erVO = mBO.createServiceMonitor(srVO);
+		Assert.assertEquals("Monitoring did not detect invalid time interval parameter", 3001, erVO.getCode());
+	}
+	
+	@Test
+	public void testStopMonitoringScenarios() {
+		srVO = new StartRequestVO();
+		srVO.setServiceURL("Service1");
+		srVO.setInterval(2);
+		mBO = new MonitoringBO();
+		erVO = new ExecutionResponseVO();
+		erVO = mBO.stopMonitoring(srVO.getServiceURL());
+		Assert.assertEquals("Monitorind did not detect that no history exists", 2003, erVO.getCode());
+		mBO.createServiceMonitor(srVO);
+		mBO.stopMonitoring(srVO.getServiceURL());
+		erVO = mBO.stopMonitoring(srVO.getServiceURL());
+		Assert.assertEquals("Monitoring did not detect duplicate stop request", 2002, erVO.getCode());
+	}
 }
+
